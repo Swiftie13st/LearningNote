ee8aed4c923232ff5331406c646050008baa5f0b
Bruce <swiftie13s@163.com>
2022-06-15 15:26:04 +0800

    package

--- goProject/main.go
+++ goProject/main.go
@@ -3,8 +3,29 @@ package main
 import (
 	"fmt"
 	"goProject/calc"
+
+	"github.com/shopspring/decimal"
+	"github.com/tidwall/gjson"
 )
 
+const json = `{"name":{"first":"Janet","last":"Prichard"},"age":47}`
+
 func main() {
-	fmt.Printf("%v", calc.Add(2, 5))
+	fmt.Printf("%v \n", calc.Add(2, 5))
+	// 打印公有变量
+	fmt.Println(calc.Name)
+
+	_, err := decimal.NewFromString("136.02")
+	if err != nil {
+		panic(err)
+	}
+
+	var num1 float64 = 3.1
+	var num2 float64 = 4.2
+	d1 := decimal.NewFromFloat(num1).Add(decimal.NewFromFloat(num2))
+	fmt.Println(d1)
+
+	value := gjson.Get(json, "name.last")
+	println(value.String())
+
 }
