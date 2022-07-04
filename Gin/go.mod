71e89865ae7e8bbd7b4e38be55b86b192cd0d7c7
Bruce <swiftie13s@163.com>
2022-07-04 19:15:22 +0800

    go JWT

--- Gin/go.mod
+++ Gin/go.mod
@@ -9,6 +9,9 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
--- Gin/main.go
+++ Gin/main.go
@@ -1,109 +1,95 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+
+// 	"github.com/golang-jwt/jwt"
+// )
+
+// func main() {
+// 	// hmacSampleSecret := []byte("111") //密钥，不能泄露
+// 	// //生成token对象
+// 	// token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+// 	// 	"foo": "bar",
+// 	// 	"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+// 	// })
+// 	// //生成jwt字符串
+// 	// tokenString, err := token.SignedString(hmacSampleSecret)
+// 	// fmt.Println(tokenString, err)
+
+// 	hmacSampleSecret := []byte("111")
+// 	token := jwt.New(jwt.SigningMethodHS256)
+// 	//通过New方法不能在创建的时候携带数据，因此可以通过给token.Claims赋值来定义数据
+// 	token.Claims = jwt.MapClaims{
+// 		"foo": "bar",
+// 		"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+// 	}
+// 	tokenString, err := token.SignedString(hmacSampleSecret)
+// 	fmt.Println(tokenString, err)
+// }
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/golang-jwt/jwt"
+// )
+
+// type CustomerClaims struct {
+// 	Username string `json:"username"`
+// 	Gender   string `json:"gender"`
+// 	Avatar   string `json:"avatar"`
+// 	Email    string `json:"email"`
+// }
+
+// func (c CustomerClaims) Valid() error {
+// 	return nil
+// }
+
+// func main() {
+// 	//密钥
+// 	hmacSampleSecret := []byte("111")
+// 	token := jwt.New(jwt.SigningMethodHS256)
+// 	token.Claims = CustomerClaims{
+// 		Username: "张三",
+// 		Gender:   "男",
+// 		Avatar:   "https://1.jpg",
+// 		Email:    "test@163.com",
+// 	}
+// 	tokenString, err := token.SignedString(hmacSampleSecret)
+// 	fmt.Println(tokenString, err)
+// }
+
 package main
 
 import (
 	"fmt"
-	"net/http"
-	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
-// HandlerFunc
-func indexHandler(c *gin.Context) {
-	fmt.Println("index")
-	name, ok := c.Get("name")
-
-	if !ok {
-		name = "default"
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"msg": name,
-	})
-}
-
-// 定义一个中间件m1：统计请求处理函数的执行时间
-func m1(c *gin.Context) {
-	fmt.Println("m1 in ...")
-
-	start := time.Now() //计时
-	// go funcXX(c.Copy()) // 在funcXX中只能使用c的拷贝
-	c.Next() //调用后续的处理函数
-	// c.Abort() // 阻止调用后续的处理函数
-	cost := time.Since(start)
-
-	fmt.Printf("cost : %v\n", cost)
-	fmt.Println("m1 out ...")
-}
-
-// 定义一个中间件m2
-func m2(c *gin.Context) {
-	fmt.Println("m2 in ...")
-
-	c.Set("name", "Bruce")
-
-	c.Next() //调用后续的处理函数
-	// c.Abort() // 阻止调用后续的处理函数
-	fmt.Println("m2 out ...")
-}
-
-func authMiddleware(doCheck bool) gin.HandlerFunc {
-	// 链接数据库
-	// 等准备工作
-	return func(c *gin.Context) {
-		// 存放具体的逻辑
-		if doCheck {
-			// 如：是否登录的判断
-			// if 登录
-			// c.Next()
-			// else
-			// c.Abort()
-		} else {
-			c.Next()
-		}
-
-	}
+type CustomerClaims struct {
+	Username string `json:"username"`
+	Gender   string `json:"gender"`
+	Avatar   string `json:"avatar"`
+	Email    string `json:"email"`
+	jwt.StandardClaims
 }
 
 func main() {
-	r := gin.Default()
+	var hmacSampleSecret = []byte("111")
+	//前面例子生成的token
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IuW8oOS4iSIsImdlbmRlciI6IueUtyIsImF2YXRhciI6Imh0dHBzOi8vMS5qcGciLCJlbWFpbCI6InRlc3RAMTYzLmNvbSJ9.N-ZFu253w9PxQmuYbaCK-9DcvdfB74tH-B4-6tchjXc"
 
-	r.Use(m1, m2, authMiddleware(true)) // 全局注册中间件函数m1, m2
-
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World!")
+	token, err := jwt.ParseWithClaims(tokenString, &CustomerClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return hmacSampleSecret, nil
 	})
 
-	r.GET("/index", indexHandler)
-
-	r.GET("/user", func(c *gin.Context) {
-		fmt.Println("user")
-		c.JSON(http.StatusOK, gin.H{
-			"msg": "user",
-		})
-	})
-
-	// 为路由组注册中间件方法
-	// videoGroup := r.Group("/video", m1)
-	videoGroup := r.Group("/video")
-	videoGroup.Use(m1)
-	{
-		videoGroup.GET("/index", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"msg": "/video/index",
-			})
-		})
-		videoGroup.GET("/about", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"msg": "/video/about",
-			})
-		})
-		videoGroup.GET("/home", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"msg": "/video/home",
-			})
-		})
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-
-	r.Run(":9003") // listen and serve on 0.0.0.0:PORT(default:8080)
-
+	claims := token.Claims.(*CustomerClaims)
+	fmt.Println(claims)
 }
