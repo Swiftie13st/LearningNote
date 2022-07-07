85f7a14d21f2a9d714cec29e2e7d0c97fa6260d2
Bruce <swiftie13s@163.com>
2022-07-07 09:58:19 +0800

    tcp

--- advanceCode/goroutine/main.go
+++ advanceCode/goroutine/main.go
@@ -8,11 +8,15 @@ import (
 var wg sync.WaitGroup
 
 func hello(i int) {
+	// fmt.Println("Hello Goroutine!")
 	defer wg.Done() // goroutine结束就登记-1
 	fmt.Println("Hello Goroutine!", i)
 }
 func main() {
 
+	// go hello() // 启动另外一个goroutine去执行hello函数
+	// fmt.Println("main goroutine done!")
+	// time.Sleep(time.Second)
 	for i := 0; i < 10; i++ {
 		wg.Add(1) // 启动一个goroutine就登记+1
 		go hello(i)
