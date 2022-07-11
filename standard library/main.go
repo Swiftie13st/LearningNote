190522abc6bfeffea9a66a705441e9cfb005f82e
Bruce <swiftie13s@163.com>
2022-07-11 15:01:48 +0800

    flag

---
+++ standard library/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+//os.Args demo
+func main() {
+	//os.Args是一个[]string
+	if len(os.Args) > 0 {
+		for index, arg := range os.Args {
+			fmt.Printf("args[%d]=%v\n", index, arg)
+		}
+	}
+}
